912b24022d3a84e2885bdd11b659a80f2a0ff182
Eve Redero <eve.redero@gmail.com>
2026-07-04 14:27:10 +0200

    [DBG] FT2232 debug and flash

--- boards/arm/coffeecaller_nrf52/board.cmake
+++ boards/arm/coffeecaller_nrf52/board.cmake
@@ -1,5 +1,4 @@
 # Copyright (c) 2025 TiaC Systems
 # SPDX-License-Identifier: Apache-2.0
 
-board_runner_args(openocd "--config=${BOARD_DIR}/../../coffeecaller_ftdi2232.cfg")
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
